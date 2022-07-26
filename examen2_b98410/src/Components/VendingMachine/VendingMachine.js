69fd3b370772eb91a129b68618e69d229375984a
JAROD DANIEL VENEGAS ALPIZAR <jarod.venegas@ucr.ac.cr>
2022-07-26 17:48:25 -0600

    Se agregaron los componentes para ver los refresco y para elegirlos a al componente principal llamado Vending Machine

--- examen2_b98410/src/Components/VendingMachine/VendingMachine.js
+++ examen2_b98410/src/Components/VendingMachine/VendingMachine.js
@@ -1,16 +1,33 @@
-import { React } from "react";
+import { React, useState } from "react";
 import "./VendingMachine.css";
+import { SodasComp } from "../SodasComp/SodasComp";
+import { SodasTypeInput } from "../SodasTypeInput/SodasTypeInput";
 
 export const VendingMachine = () => {
-
+  const [totalCost, setTotalCost] = useState(0);
+  const [pay, setPay] = useState(false);
+  const [updateSodaStock, setUpdateSodaStock] = useState(false);
+  const [reset, setReset] = useState(false);
+  
   return (
     <>
     <div className="name">Jarod Venegas Alpizar - B98410</div>
       <div className="vending-machine">
         <div className="vending-machine-top">
           <div className="vending-machine-sodas">
+          <SodasComp
+              updateSodaStock={updateSodaStock}
+              setUpdateSodaStock={setUpdateSodaStock}
+            />
           </div>
           <div className="vending-machine-panel">
+          <SodasTypeInput
+              setTotalCost={setTotalCost}
+              pay={pay}
+              setPay={setPay}
+              setUpdateSodaStock={setUpdateSodaStock}
+              reset={reset}
+            />
           </div>
         </div>
           <div className="vending-machine-dispenser"></div>
