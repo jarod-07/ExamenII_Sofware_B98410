4ba8e7678db147028cdf8d5e881576d43b815fd1
JAROD DANIEL VENEGAS ALPIZAR <jarod.venegas@ucr.ac.cr>
2022-07-26 17:39:52 -0600

    Implementacion de la base de la pagina

--- examen2_b98410/src/App.js
+++ examen2_b98410/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { VendingMachine } from './Components/VendingMachine/VendingMachine';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <VendingMachine/>
       </header>
     </div>
   );
---
+++ examen2_b98410/src/Components/VendingMachine/VendingMachine.js
@@ -0,0 +1,20 @@
+import { React } from "react";
+import "./VendingMachine.css";
+
+export const VendingMachine = () => {
+
+  return (
+    <>
+    <div className="name">Jarod Venegas Alpizar - B98410</div>
+      <div className="vending-machine">
+        <div className="vending-machine-top">
+          <div className="vending-machine-sodas">
+          </div>
+          <div className="vending-machine-panel">
+          </div>
+        </div>
+          <div className="vending-machine-dispenser"></div>
+      </div>
+    </>
+  );
+};
