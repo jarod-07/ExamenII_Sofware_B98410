0d80b6352dacbab4f3496ced0f14ff21fd1ad2ca
JAROD DANIEL VENEGAS ALPIZAR <jarod.venegas@ucr.ac.cr>
2022-07-26 17:51:29 -0600

    Cambios en el componete Vending Machine

--- examen2_b98410/src/Components/VendingMachine/VendingMachine.js
+++ examen2_b98410/src/Components/VendingMachine/VendingMachine.js
@@ -1,13 +1,9 @@
 import { React, useState } from "react";
 import "./VendingMachine.css";
 import { SodasComp } from "../SodasComp/SodasComp";
-import { SodasTypeInput } from "../SodasTypeInput/SodasTypeInput";
 
 export const VendingMachine = () => {
-  const [totalCost, setTotalCost] = useState(0);
-  const [pay, setPay] = useState(false);
   const [updateSodaStock, setUpdateSodaStock] = useState(false);
-  const [reset, setReset] = useState(false);
   
   return (
     <>
@@ -21,13 +17,6 @@ export const VendingMachine = () => {
             />
           </div>
           <div className="vending-machine-panel">
-          <SodasTypeInput
-              setTotalCost={setTotalCost}
-              pay={pay}
-              setPay={setPay}
-              setUpdateSodaStock={setUpdateSodaStock}
-              reset={reset}
-            />
           </div>
         </div>
           <div className="vending-machine-dispenser"></div>
