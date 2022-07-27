74afc7b5c6080ff5da65546c4e1ec3490cec963f
JAROD DANIEL VENEGAS ALPIZAR <jarod.venegas@ucr.ac.cr>
2022-07-26 18:01:35 -0600

    Informacion necesaria que funciona como backend para poder correr la aplicacion y simular su funcionamiento

---
+++ examen2_b98410/src/Data/ChangeInfo.js
@@ -0,0 +1,19 @@
+export const changeInfo = 
+[
+  {
+    coin: 500,
+    count: 20
+  },
+  {
+    coin: 100,
+    count: 30
+  },
+  {
+    coin: 50,
+    count: 50
+  },
+  {
+    coin: 25,
+    count: 25
+  },
+]
